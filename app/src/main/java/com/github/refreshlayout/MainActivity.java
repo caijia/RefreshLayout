ec116b4be247a3d7c23db76089f14043dea1f6fa
caijia <android_nihao@163.com>
2017-03-04 17:37:16 +0800

    test dev branch

--- app/src/main/java/com/github/refreshlayout/MainActivity.java
+++ app/src/main/java/com/github/refreshlayout/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity implements RefreshLayout.OnR
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new TestPullAdapter());
 
-//        refreshLayout.setHeaderViewId(R.layout.header);
+//        refreshLayout.setHeaderViewId(R.layout.header);d
         refreshLayout.setOnRefreshListener(this);
     }
 
